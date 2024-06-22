faa0c297576b444a1b3508230f1db4ff51d0f39b
taoye <596296093@qq.com>
2024-06-22 16:32:27 +0800

    leecode链表 20240622

---
+++ algorithm/src/main/java/leecode/link/LM_1171.java
@@ -0,0 +1,54 @@
+package leecode.link;
+
+import lombok.val;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LM_1171 {
+
+    public ListNode removeZeroSumSublists(ListNode head) {
+        if (head == null) return null;
+        int sum = 0;
+        for (ListNode h = head; h != null; h = h.next) {
+            sum += h.val;
+            if(sum == 0) {
+                return removeZeroSumSublists(h.next);
+            }
+        }
+        head.next = removeZeroSumSublists(head.next);
+        return head;
+    }
+
+    public ListNode removeZeroSumSublists1(ListNode head) {
+        Map<Integer, ListNode> map = new HashMap<>();   //<前缀和,节点>
+        ListNode dmy = new ListNode(0, head);   //因为要删除 总和 值为 0 的连续节点组成的序列，所以dmy的val设置为0
+        ListNode cur = dmy;  // 必须从伪节点开始遍历，不然碰到[-1,1]这种会删除不了
+        int preSum = 0;       // 记录的前缀和
+        //一次遍历
+        while (cur != null) {
+            preSum += cur.val;
+            map.put(preSum, cur);
+            cur = cur.next;
+        }
+        //二次遍历
+        cur = dmy;
+        preSum = 0;
+        while (cur != null) {
+            preSum += cur.val;
+            if (map.containsKey(preSum)) {
+                cur.next = map.get(preSum).next; //即便map中的映射节点是cur，该条也没问题
+            }
+            cur = cur.next;
+        }
+        return dmy.next;
+    }
+
+    public static void main(String[] args) {
+        int[] case1 = new int[] {1,2,-3,3,1};
+        ListNode case1List = new ListNode(case1);
+        ListNode listNode = new LM_1171().removeZeroSumSublists(case1List);
+        System.out.println(listNode);
+    }
+
+}
--- algorithm/src/main/java/leecode/link/LM_2487.java
+++ algorithm/src/main/java/leecode/link/LM_2487.java
@@ -1,6 +1,28 @@
 package leecode.link;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class LM_2487 {
+
+    public ListNode removeNodes(ListNode head) {
+        Deque<ListNode> stack = new ArrayDeque<>();
+        while(head != null) {
+            while(!stack.isEmpty() && stack.peek().val < head.val) {
+                stack.pop();
+            }
+            stack.push(head);
+            head = head.next;
+        }
+        ListNode nx = null;
+        while (!stack.isEmpty()) {
+            ListNode cur = stack.pop();
+            cur.next = nx;
+            nx = cur;
+        }
+        return nx;
+    }
+
     /**
      * 从右往左找非递减序列
      *
@@ -9,9 +31,9 @@ public class LM_2487 {
      * 空间复杂度：O(n)，需要 O(n)的栈空间。
      * 递归本质就是在倒着遍历链表
      */
-    public ListNode removeNodes(ListNode head) {
+    public ListNode removeNodes1(ListNode head) {
         if(head == null || head.next == null) return head;
-        ListNode subHead = removeNodes(head.next);
+        ListNode subHead = removeNodes1(head.next);
         if(head.val < subHead.val) {
             return subHead;
         }
